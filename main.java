5f2379f85d073ee8d8cc2b01d0e0771c0674f0d7
Santi <santi123@pro.me>
2024-09-30 23:42:00 -0600

    "Java aprend"

---
+++ main.java
@@ -0,0 +1,19 @@
+
+public class main {
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 32767;
+        int a = 40000;
+        long l = 1000000000; // 1 billion de datos
+        float f = 3.1416f; //decimal
+        double d = 3.1416; //decimal
+        char c = 'a'; //caracter solo una letra
+        boolean bo = true; //true or false
+        
+
+ 
+        b = 80;
+        System.out.println(b);
+    }
+    
+}   
