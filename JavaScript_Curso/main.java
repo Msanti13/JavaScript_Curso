9e7babb5367b8d1c0f1fcf201d6e6494c6c6b43f
maik <maik@hupa.com>
2024-10-06 22:38:52 -0600

    operadores

--- JavaScript_Curso/main.java
+++ JavaScript_Curso/main.java
@@ -9,11 +9,31 @@ public class main {
         double d = 3.1416; //decimal
         char c = 'a'; //caracter solo una letra
         boolean bo = true; //true or false
-        
-
- 
         b = 80;
-        System.out.println(b);
+        //Constantes con final
+        final byte  M = 80; //diferencia con variable que no se puede moficar su valor
+                
+        System.out.println("esto es una vuiable: " + b);
+        System.out.println("esto es una constante: "+ M);
+
+        //Operadores
+        int x = 5;
+        int y = 2;
+        int z = x + y;
+        int w = x - y;
+        int q = x * y;
+        int r = x / y;
+        System.out.println("Operadoes");
+        System.out.println("La suma es: " + z);
+        System.out.println("La resta es: " + w);
+        System.out.println("La multiplicacion es: " + q);
+        System.out.println("La division es: " + r);
+        System.out.println("Mayo o no: "+ (x>y));
+        System.out.println("Menor o no: "+ (x<y));
+        System.out.println("Igual o no: "+ (x==y));
+        System.out.println("Diferente o no: "+ (x!=y));
+
+
     }
     
 }   
