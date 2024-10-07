4dfd10b7c9212e36250f05687af40529db06a881
maik <maik@hupa.com>
2024-10-06 22:07:59 -0600

    conversion

--- JavaScript_Curso/JavaScript/2tipos.js
+++ JavaScript_Curso/JavaScript/2tipos.js
@@ -35,3 +35,56 @@ console.log(`Typo de array: ${typeof(arr1)} and arr2: ${arr2}`);
 arr2.sort();
 console.log("sorted arr2:",arr2);
 console.log("lenght of arr2:",arr2.length);
+
+//Conversion Tipos
+//string
+
+var str = "10";
+var boolen = true;
+var nul = null;
+var undef = undefined;
+
+var str_num = new String(str).valueOf();
+var str_bool = new String(boolen).valueOf();
+var str_null = new String(nul).valueOf();
+var str_undef = new String(undef).valueOf();
+
+console.log("str:". str_num,"- type:", typeof(str_num));
+console.log("boolen:", str_bool,"- type:", typeof(str_bool));
+console.log("null:", str_null,"- type:", typeof(str_null));
+console.log("undef:", str_undef,"- type:", typeof(str_undef));
+
+//boleean
+var numm = 8;
+var ss = "true";
+var nulll = null;
+var undefi = undefined;
+
+var bool_num = new Boolean(numm).valueOf();
+var bool_str = new Boolean(ss).valueOf();
+var bool_null = new Boolean(nulll).valueOf();
+var bool_undef = new Boolean(undefi).valueOf();
+
+console.log("numm:", bool_num,"- type:", typeof(bool_num));
+console.log("ss:", bool_str,"- type:", typeof(bool_str));
+console.log("nulll:", bool_null,"- type:", typeof(bool_null));
+console.log("undefi:", bool_undef,"- type:", typeof(bool_undef));
+
+
+//number
+var sss = '11';
+var bo = true;
+var parse = "10.5";
+var float = 10.5;
+
+
+var num_sss = new Number(sss).valueOf();
+var num_bo = new Number(bo).valueOf();
+var num_parse = parseInt(parse).valueOf();
+var num_float = parseFloat(float).valueOf();
+
+console.log("sss:", num_sss,"- type:", typeof(num_sss));
+console.log("bo:", num_bo,"- type:", typeof(num_bo));
+console.log("parse:", num_parse,"- type:", typeof(num_parse));
+console.log("float:", num_float,"- type:", typeof(num_float));
+
