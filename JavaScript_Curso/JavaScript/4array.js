37c043b44853552dff7e1ddb4abad775cd9f016f
Santi <santi123@pro.me>
2024-10-14 21:48:58 -0600

    condicionales y array

---
+++ JavaScript_Curso/JavaScript/4array.js
@@ -0,0 +1,6 @@
+//creando matrices
+var arr = new Array(); //creando un array vacio
+var arr1 = []; //creando un array vacio
+var arr2 = new Array(1, 2, "Hola"); //creando un array con elementos
+var arr3 = [12]; //creando un array con elementos 12
+console.log(arr, arr1, arr2, arr3);
