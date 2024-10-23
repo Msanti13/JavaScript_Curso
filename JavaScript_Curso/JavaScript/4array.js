d4c4cfc7f11f5cf47199fc353382b59a2eadb6c0
maik <maik@hupa.com>
2024-10-22 23:13:42 -0600

    metodos

--- JavaScript_Curso/JavaScript/4array.js
+++ JavaScript_Curso/JavaScript/4array.js
@@ -26,7 +26,56 @@ console.log("Copia de una matriz con spread operator:", arracopy2);
 
 
 //Medotos y propiedades de las matrices
-//length
+
+
+//propiedad length
 console.log("Popiedad length:");
 console.log("Propiedad length:", arr2.length);
 console.log("Propiedad length:", arr3.length);
+
+//propiedad prototype
+var arr6 = [1, 2, 3];
+console.log("Propiedad prototype:", arr6.prototype); //undefined
+arr6.prototype = "Hola";
+console.log("Propiedad prototype:", arr6.prototype); //Hola
+console.log("Propiedad prototype:", arr6);
+
+//Metodos de las matrices
+//Metodo push()
+console.log("Metodo push():");
+var arr7 = [1, 2, 3];
+arr7.push(4);
+console.log("Metodo push():", arr7);
+
+//Metodo pop()
+console.log("Metodo pop():");
+var arr8 = [1, 2, 3];
+arr8.pop();
+console.log("Metodo pop():", arr8); //1,2 elimina el último elemento de la array y lo devuelve a ser asignado a una variable.
+
+//Metodo shift()
+console.log("Metodo shift():");
+var arr9 = [1, 2, 3];
+arr9.shift();
+console.log("Metodo shift():", arr9); //2,3 elimina el primer elemento de la array y lo devuelve a ser asignado a una variable.
+
+//Metodo unshift()
+console.log("Metodo unshift():");
+var arr10 = [1, 2, 3];
+arr10.unshift(0);
+console.log("Metodo unshift():", arr10); //0,1,2,3 añade un elemento al principio de la array.
+
+//Metodo indexOf()
+console.log("Metodo indexOf():");
+var arr11 = [1, 2, 3];
+console.log("Metodo indexOf():", arr11.indexOf(2)); //1 devuelve el índice del primer elemento coincidente en la matriz.
+
+//Metodo toString()
+console.log("Metodo toString():");
+var arr12 = [1, 2, 3];
+var arr13 = arr12.toString();
+console.log("Metodo toString():", arr13, typeof arr13); //1,2,3 convierte una matriz en una cadena de texto.
+
+
+
+
