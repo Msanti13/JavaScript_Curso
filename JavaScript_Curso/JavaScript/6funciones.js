95d16832c2eec5299d1906d6545db1325312ab5f
Santi <santi123@pro.me>
2024-11-22 22:25:29 -0600

    wwwwww

--- JavaScript_Curso/JavaScript/6funciones.js
+++ JavaScript_Curso/JavaScript/6funciones.js
@@ -25,5 +25,80 @@ function resta(a, b) {
 
 console.log(resta(10, 5)); //5
 
-//otro ejemplo
+//FUNCIONES FLECHA
+//Las funciones de flecha son una forma más corta de escribir funciones.
+var myFunction1 = () => {
+    console.log("Hola Mundo js!");
+}
+myFunction1(); //Hola Mundo js! 
 
+
+var suma1 = (a, b) => a + b;
+console.log("La suma es ", suma1(5, 10)); //15
+
+var funcc = () => console.log("Funcion sin argumentos!");
+funcc(); //Funcion sin argumentos!
+
+//Funcion de flecha devolviendo un objeto
+var objeto = () => ({ nombre: "Juan", edad: 25 });
+console.log(objeto()); //{ nombre: 'Juan', edad: 25 }
+
+//funcion como argumento
+function saludar() {
+    return "Hola";
+}
+
+function saludar1(funcion) {
+    return funcion();
+}
+
+console.log(saludar1(saludar)); //Hola
+
+//Funciones de flecha como argumento
+var saludar2 = () => "Hola";
+var saludar3 = (funcion) => funcion();
+console.log(saludar3(saludar2)); //Hola
+
+
+//funcion como objeto de retorno
+function objeto1() {
+    return function() {
+        return "Hola desde un objeto";
+    }
+}
+var fun = objeto1();
+console.log(fun()); //Hola
+
+//Funciones de flecha como objeto de retorno
+var objeto2 = () => () => "Hola desde un objeto2";
+var fun1 = objeto2();
+console.log(fun1()); //Hola desde un objeto
+
+//metodos de funciones con forEach
+var numeros = [1, 2, 3, 4, 5];
+numeros.forEach(function(numero) {
+    console.log(numero);
+  
+});
+
+console.log("Fin del programa");
+
+//otro ejemplo
+var arr = [10, 8, 33, 40, 51];
+arr.forEach((val, index, array) => {
+    console.log(val, index, array);
+});
+
+//metodo filter
+var filt = [10, 20, 30, 40, 50];
+var res2 = filt.filter(a => a > 30);
+console.log(filt); //[ 40, 50 ]
+console.log(res2); //[ 40, 50 ] 
+
+
+
+
+
+var arr1 = [10, 8, 33, 40, 51];
+var result = arr1.filter((val) => val > 30);
+console.log(result); //[ 33, 40, 51 ]
